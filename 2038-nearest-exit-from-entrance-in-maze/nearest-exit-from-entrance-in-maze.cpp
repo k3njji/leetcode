f5654d08ca872abb4cdbb10d9b0e41abaec1a12d
k3njji <118595017+k3njji@users.noreply.github.com>
2026-02-06 15:52:43 +0700

    Time: 4 ms (79.15%) | Memory: 32.7 MB (99.54%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -30,7 +30,51 @@ public:
             minPq.push({step+1, {entrance[0], right}});
         }
 
-        bfs(maze, minPq, flag, step);
+        while(!minPq.empty()){
+            pair<int, pair<int, int>> tops = minPq.top();
+
+            step = tops.first;
+            // cout << step<< endl;
+
+            int x = tops.second.first;
+            int y = tops.second.second;
+
+            maze[x][y] = '+';
+
+            int top = x-1;
+            int bot = x+1;
+            int left = y-1;
+            int right = y+1;
+
+            if(x == 0 || x == maze.size()-1 || y == 0 || y == maze[0].size() - 1){
+                flag = true;
+                break;
+            }
+
+            if(top >= 0 && maze[top][y] == '.'){
+                maze[top][y] = '+';
+                minPq.push({step+1, {top, y}});
+            }
+            
+            if(bot < maze.size() && maze[bot][y] == '.'){
+                maze[bot][y] = '+';
+                minPq.push({step+1, {bot, y}});
+            }
+
+            if(left >= 0 &&  maze[x][left] == '.'){
+                maze[x][left] = '+';
+                minPq.push({step+1, {x, left}});
+
+            }
+
+            if(right < maze[0].size() &&  maze[x][right] == '.'){
+                maze[x][right] = '+';
+
+                minPq.push({step+1, {x, right}});
+            }
+            minPq.pop();
+        }
+    
         
         if(flag){
             return step;
