4a283c45e7b620c729a5ee24a3618410bbd79dc0
k3njji <118595017+k3njji@users.noreply.github.com>
2026-02-12 12:33:53 +0700

    Time: 491 ms (6.20%) | Memory: 41.6 MB (13.62%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int ans = 0;
+
+        for(int i = 0; i < s.size(); i++){
+            unordered_map<int, int>freq;
+            int unique = 0;
+            int maxFreq = 0;
+            for(int j = i; j < s.size(); j++){
+                
+                freq[s[j]-'a']++;
+                if(freq[s[j]-'a'] == 1){
+                    unique++;
+                }
+                if(maxFreq < freq[s[j]-'a']){
+                    maxFreq = freq[s[j]-'a'];
+                }
+                int total = 0;
+                int curr = j-i+1;
+
+                for(auto& x: freq){
+                    total = total+x.second;
+                }
+
+                if(total == unique*maxFreq && total == curr){
+                    ans = max(ans, curr);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
