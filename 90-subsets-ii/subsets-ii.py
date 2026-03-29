eee22173ea1ea0b1f4d32d2641f30dd83cdcf5d0
k3njji <118595017+k3njji@users.noreply.github.com>
2026-03-30 01:21:11 +0700

    Time: 3 ms (20.85%) | Memory: 19.8 MB (16.41%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        res = set()
+
+        def backtrack(build, curr):
+            if len(build) > len(nums):
+                return
+            
+            key = tuple(sorted(build))
+            if len(build) <= len(nums) and key not in res:
+                ans.append(build)
+                res.add(key)
+            
+            for i in range(curr, len(nums)):
+                backtrack(build + [nums[i]], i + 1)
+
+        backtrack([], 0)
+        return ans
