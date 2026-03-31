8db339a85affa4fcfacbc494cce92fc1ddd2a701
k3njji <118595017+k3njji@users.noreply.github.com>
2026-03-31 21:20:54 +0700

    Time: 3 ms (90.90%) | Memory: 20.5 MB (66.67%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -1,23 +1,17 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         ans = []
-        # res = set()
-        def btrack(words, curr):
-            # print(words)
-            if(len(words) == len(s)):
-                ans.append(words)
-                # res.add(words)
-                return
-            for i in range(curr, len(s)):
-                if(s[i].isalpha()):
-                    btrack(words+s[i].lower(), i+1)
-                    btrack(words+s[i].upper(), i+1)
-                else:
-                    btrack(words+s[i], i+1)
 
+        def btrack(words, curr):
+            if curr == len(s):
+                ans.append(words)
+                return
+            
+            if s[curr].isalpha():
+                btrack(words + s[curr].lower(), curr + 1)
+                btrack(words + s[curr].upper(), curr + 1)
+            else:
+                btrack(words + s[curr], curr + 1)
 
         btrack("", 0)
-
         return ans
-            
-        
