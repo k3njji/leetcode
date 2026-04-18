fa862ab36806ed4bab9a78ee7bdb9cdbd99993cf
k3njji <118595017+k3njji@users.noreply.github.com>
2026-04-18 20:08:19 +0700

    Time: 5 ms (0.45%) | Memory: 8.5 MB (60.81%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int prevese = n;
+        int reverse = 0;
+
+        while(n > 0){
+            reverse*=10;
+            int temp = n%10;
+            reverse += temp;
+            cout << reverse << endl;
+            n = n/10;
+        }
+
+        return abs(prevese - reverse);
+    }
+};
